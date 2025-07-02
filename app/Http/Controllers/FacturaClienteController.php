2bb5c06628d4490558b0e2460fdfd7b533bd63fb
LfullStack <quinterolevii87@gmail.com>
2025-07-01 20:21:11 -0500

    feat: Actualizar facturas PDF con nuevos datos y formatos

--- app/Http/Controllers/FacturaClienteController.php
+++ app/Http/Controllers/FacturaClienteController.php
@@ -1,23 +1,25 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\FacturaCliente;
 
+use App\Models\FacturaCliente;
 use Illuminate\Support\Facades\Auth;
 
 class FacturaClienteController extends Controller
 {
     public function index()
     {
-        // Obtener el ID del cliente autenticado
         $clienteId = Auth::id();
 
-        // Obtener las facturas del cliente autenticado
         $facturas = FacturaCliente::where('cliente_id', $clienteId)
             ->with(['productos'])
             ->get();
 
-        // Retornar la vista con las facturas
         return view('admin.reportes.facturas-clientes.index', compact('facturas'));
+    }
+
+    public function exportar()
+    {
+        return response()->json(['mensaje' => 'Exportando facturas de cliente...']);
     }
 }
