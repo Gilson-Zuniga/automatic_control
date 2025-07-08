6735fd65924c2cddcc266b94dd6cb0652cdeea03
Gilson Zuñiga <gilsonzuniga@gmail.com>
2025-07-08 14:51:04 -0500

    Correcion de estructura en head

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -266,8 +266,8 @@
         <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
         <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
         
-        @stack('scripts')
-        @fluxScripts
+
+        
 
         @if (session('swal'))
 
@@ -276,5 +276,7 @@
             </script>
             
         @endif
+                @stack('scripts')
+        @fluxScripts
     </body>
 </html>
