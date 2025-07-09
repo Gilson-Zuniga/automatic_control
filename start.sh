9f65bb409e11dd80976043a65ddc1744d9b175e7
Gilson Zuñiga <gilsonzuniga@gmail.com>
2025-07-08 23:55:57 -0500

    Refactor start.sh: mejora en la secuencia de comandos y limpieza de configuración

--- start.sh
+++ start.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 
-# Copia .env si no existe
 if [ ! -f .env ]; then
   cp .env.example .env
 fi
 
-# Genera clave de la app
+php artisan config:clear
+php artisan cache:clear
+php artisan config:cache
+
 php artisan key:generate --force
-
-# Ejecuta migraciones obligatorias
 php artisan migrate --force
-
-# Inicia el servidor Laravel
 php artisan serve --host=0.0.0.0 --port=8000
-chmod +x start.sh
