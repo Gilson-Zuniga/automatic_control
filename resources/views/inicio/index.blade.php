051f15d7e8a4d99dce58fe9d12c0fbed8bfd87ae
Levi <quinterolevii87@gmail.com>
2025-08-25 15:23:33 -0500

    Add favicon link to the head section of index.blade.php

--- resources/views/inicio/index.blade.php
+++ resources/views/inicio/index.blade.php
@@ -12,6 +12,8 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700&display=swap" rel="stylesheet">
   <link id="app-style" href="{{ asset('css/inicio.css') }}" rel="stylesheet">
+  <link rel="icon" type="image/png" href="{{ asset('img/logo.png') }}">
+
 
   <script type="importmap">
 {
